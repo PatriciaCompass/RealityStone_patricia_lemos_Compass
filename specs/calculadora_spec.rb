f77a11066634c59ced6f597f595ea95f17152662
PatriciaCompass <patricia.lemos@compasso.com.br>
2022-07-29 16:48:51 -0300

    Correção na nomenclatura de métodos

--- specs/calculadora_spec.rb
+++ specs/calculadora_spec.rb
@@ -1,31 +1,31 @@
 require 'rspec'
 require 'pry'
-require_relative '..src/calculadora'
+require_relative '../src/calculadora'
 
 describe('Calculadora') do
     calculadora = Calculadora.new
 
     it ('Deve somar dois inteiros positivos') do
-        expect(Calculadora.soma(4, 5)).to eq 9
+        expect(calculadora.soma(4, 5)).to eq 9
     end
 
     it ('Deve somar um número qualquer com 0') do
-        expect(Calculadora.soma(9, 0)).to eq 9
+        expect(calculadora.soma(9, 0)).to eq 9
     end
 
     it ('Deve somar número negativos') do
-        expect(Calculadora.soma(-9, -10)).to eq -10
+        expect(calculadora.soma(-9, -10)).to eq -19
     end
 
     it ('Deve subtrair dois números inteiros positivos') do
-        expect(Calculadora.subtrai(6, 2)).to eq 8
+        expect(calculadora.subtrai(6, 2)).to eq 4
     end
 
     it ('Deve multiplicar os números') do
-        expect(Calculadora.multiplique(14, 9)).to eq 126
+        expect(calculadora.multiplique(14, 9)).to eq 126
     end
 
-    it ('Deve dividir dois números') do
-        expect(Calculadora.divide(40, 5)).to eq 8
+    it ('Deve dividir os números usando o método de divisão') do
+        expect(calculadora.divide(40, 5)).to eq 8
     end
 end
